197d8640273e39e0648b73a214945a56a3559c5a
VinMoria <cyo01@outlook.com>
2025-05-03 16:26:10 +0800

    web

--- webapp/app.py
+++ webapp/app.py
@@ -16,12 +16,20 @@ def submit_data():
     try:
         # 从请求中获取 JSON 数据
         data = request.get_json()
-        
         df = pd.DataFrame([data])
+
+        def convert_if_possible(col):
+            try:
+                return pd.to_numeric(col, errors='raise')  # 如果不能转为数字会抛出异常
+            except ValueError:
+                return col.astype('category')  # 不能转换时返回原始列
+        
+        df = df.apply(lambda col: convert_if_possible(col))  # 只对可转换的列进行数字转换
         print(df.dtypes)
+        print(df)
         r = model_cal(df)
         # 返回成功响应
-        return jsonify({"message": "Success", "r": r}), 200
+        return jsonify({"message": "Success", "r": str(r)}), 200
     except Exception as e:
         print(e)
         return jsonify({"message": "Data processing failed", "error": str(e)}), 500
@@ -35,7 +43,8 @@ def result():
 def model_cal(df):
     model = pickle.load(open(MODEL_PATH, 'rb'))
     prediction = model.predict(df)
-    return prediction[0, 0]
+    # 修改为预测的第一个元素
+    return prediction[0]
 
 
 if __name__ == "__main__":
